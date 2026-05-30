63b494815dcaa8e75372e9226aced9959bcbb5c7
edtanqueno420 <edison.tanqueno@gmail.com>
2026-05-30 07:31:37 -0500

    feat: add CI/CD pipeline with GitHub Actions

--- manufacturing/views/inventory_movement.py
+++ manufacturing/views/inventory_movement.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django.shortcuts import get_object_or_404
 from django.db import transaction
 from django.db.models import Count, Sum
